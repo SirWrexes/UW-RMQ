3075a73dfa57db9e836b07b98c477c0a4f2ca241
Ludovic Fernandez <ludofernandez@msn.com>
2024-08-01 13:36:11 +0200

    Never gonna let you down

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <amqpcpp/exchangetype.h>
 #include <cstddef>
 #include <cstdint>
+#include <memory>
 #include <netinet/in.h>
 #include <uv.h>
 #include <uvw.hpp>
@@ -12,53 +13,9 @@
 #include <uvw/handle.hpp>
 #include <uvw/stream.h>
 #include <uvw/tcp.h>
+#include <uvw/util.h>
 
 #include "error.hpp"
-
-enum HostKey {
-    LOCAL,
-    RABBIT,
-
-    COUNT,
-};
-
-struct Host {
-    const std::string address;
-    const uint16_t    port;
-};
-
-inline void *get_in_addr(struct sockaddr *sa)
-{
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wcast-align"
-    if (sa->sa_family == AF_INET)
-        return &((struct sockaddr_in *) sa)->sin_addr;
-    return &((struct sockaddr_in6 *) sa)->sin6_addr;
-#pragma GCC diagnostic pop
-}
-
-const std::string resolve_host(const std::shared_ptr<uvw::loop> &loop, const std::string &address, uint16_t port)
-{
-    sockaddr_in ipv4;
-    if (uv_ip4_addr(address.c_str(), port, &ipv4) == 0)
-        return address;
-
-    sockaddr_in6 ipv6;
-    if (uv_ip6_addr(address.c_str(), port, &ipv6) == 0)
-        return address;
-
-    // Address is neither IPv4 nor IPv6, try to resolve via DNS
-    auto dnsReq = loop->resource<uvw::get_addr_info_req>();
-    auto dnsRes = dnsReq->addr_info_sync(address, std::to_string(port));
-    if (!dnsRes.first)
-        FATAL("Failed to resolve endpoint {}:{}\n", address, port);
-
-    char resolved[INET6_ADDRSTRLEN];
-    if (inet_ntop(dnsRes.second->ai_family, get_in_addr(dnsRes.second->ai_addr), resolved, dnsRes.second->ai_addrlen))
-        return {resolved};
-
-    FATAL("Failed to parse resolve address for {}:{}\n", address, port);
-}
 
 namespace Rabbit
 {
@@ -66,16 +23,7 @@ namespace Rabbit
     {
         std::shared_ptr<uvw::loop> loop;
 
-        static const Host HOSTS[HostKey::COUNT] = {
-            /* [HostKey::LOCAL] = */
-            {.address = "localhost", .port = 2323},
-
-            /* [HostKey::RABBIT] = */
-            // Currently RabbitMQ is hosted on a docker on my local machine.
-            // I have another part of the project in Rust that connected successfuly, declared queues etc without issue.
-            // Thus, I *know* the issue comes from this implementation.
-            {.address = "localhost", .port = 5672},
-        };
+        const uvw::socket_address SOCKET = {.ip = "localhost", .port = 5672};
 
         const std::string user     = "guest";
         const std::string password = "guest";
@@ -88,11 +36,11 @@ namespace Rabbit
 
         int run(void) { return loop->run(); }
 
+        void onData(AMQP::Connection *con, const char *bugger, size_t size) override;
+
         void onReady(AMQP::Connection *con) override;
 
         void onError(AMQP::Connection *con, const char *message) override;
-
-        void onData(AMQP::Connection *con, const char *buffer, size_t size) override;
 
       private:
         /** Connection handle */
@@ -100,8 +48,8 @@ namespace Rabbit
         /** LiSteN handle */
         std::shared_ptr<uvw::tcp_handle> _tcp_lsn = nullptr;
 
-        AMQP::Connection *_rmq_con  = nullptr;
-        AMQP::Channel    *_rmq_chan = nullptr;
+        std::shared_ptr<AMQP::Connection> _rmq_con  = nullptr;
+        std::shared_ptr<AMQP::Channel>    _rmq_chan = nullptr;
 
         std::vector<char> _frame = {};
     };
@@ -114,40 +62,53 @@ namespace Rabbit
         _tcp_con = Rabbit::loop->resource<uvw::tcp_handle>();
         _tcp_lsn = Rabbit::loop->resource<uvw::tcp_handle>();
 
-        _tcp_con->on<uvw::error_event>([](const uvw::error_event &event, const uvw::tcp_handle &) {
-            FATAL("[TCP Con]: Error - {}\n", event.what());
+        _tcp_lsn->on<uvw::listen_event>([](const uvw::listen_event &, uvw::tcp_handle &srv) {
+            fmt::print("LSN.Listen\n");
+            std::shared_ptr<uvw::tcp_handle> client = srv.parent().resource<uvw::tcp_handle>();
+
+            client->no_delay(true);
+            client->keep_alive(true, uvw::tcp_handle::time(60));
+            client->on<uvw::error_event>([](const uvw::error_event &ev, uvw::tcp_handle &) {
+                fmt::print("Client.Error: {}\n", ev.what());
+            });
+            client->on<uvw::close_event>([](const uvw::close_event &, uvw::tcp_handle &) {
+                fmt::print("Client.Close\n");
+            });
+            client->on<uvw::end_event>([&client](const uvw::end_event &, uvw::tcp_handle &) {
+                fmt::print("Client.End\n");
+                client->close();
+            });
+            client->on<uvw::data_event>([](const uvw::data_event &ev, uvw::tcp_handle &) {
+                // --Force linebreak in formatting--
+                fmt::print("Client.Data: {}\n", std::string(ev.data.get(), ev.data.get() + ev.length));
+            });
+            client->on<uvw::write_event>([](const uvw::write_event &, uvw::tcp_handle &) {
+                fmt::print("Client.Write\n");
+            });
+            if (srv.accept(*client) == 0)
+                client->read();
+        });
+
+        _tcp_con->on<uvw::error_event>([](const uvw::error_event &ev, uvw::tcp_handle &) {
+            FATAL("CON.Error: {}\n", ev.what());
         });
 
         _tcp_con->on<uvw::connect_event>([this](const uvw::connect_event &, uvw::tcp_handle &) {
-            fmt::print("[TCP Con]: Connect @{} | {}\n", user, password);
-            _rmq_con = new AMQP::Connection(this, AMQP::Login(user, password), "/");
+            fmt::print("CON.Connect\n");
+            _rmq_con =
+                std::shared_ptr<AMQP::Connection>(new AMQP::Connection(this, AMQP::Login("guest", "guest"), "/"));
             _tcp_con->read();
         });
 
-        _tcp_con->on<uvw::data_event>([this](const uvw::data_event &event, uvw::tcp_handle &) {
-            _frame.insert(_frame.end(), event.data.get(), event.data.get() + event.length);
+        _tcp_con->on<uvw::data_event>([this](const uvw::data_event &ev, uvw::tcp_handle &) {
+            _frame.insert(_frame.end(), ev.data.get(), ev.data.get() + ev.length);
 
             if (_rmq_con->parse(_frame.begin().base(), _frame.size()) != 0)
                 _frame.clear();
         });
 
-        _tcp_lsn->on<uvw::listen_event>([](const uvw::listen_event &, uvw::tcp_handle &srv) {
-            fmt::print("[TCP Lsn]: Listen\n");
-            std::shared_ptr<uvw::tcp_handle> client = srv.parent().resource<uvw::tcp_handle>();
-
-            srv.accept(*client);
-            client->read();
-        });
-
-        {
-            const auto &[address, port] = HOSTS[HostKey::LOCAL];
-            _tcp_lsn->bind(resolve_host(loop, address, port), port);
-        }
-
-        {
-            const auto &[address, port] = HOSTS[HostKey::RABBIT];
-            _tcp_con->connect(resolve_host(loop, address, port), port);
-        }
+        _tcp_lsn->bind({.ip = "::1", .port = 7100});
+        _tcp_con->connect({.ip = "::1", .port = 5672});
     }
 
     ConnectionHandler::~ConnectionHandler()
@@ -155,14 +116,12 @@ namespace Rabbit
         fmt::print("Destructing the connection handler\n");
         _tcp_con->close();
         _tcp_lsn->close();
-        delete _rmq_con;
-        delete _rmq_chan;
     }
 
     void ConnectionHandler::onReady(AMQP::Connection *con)
     {
         fmt::print("One day I'll see this message, I believe!\n");
-        _rmq_chan = new AMQP::Channel(con);
+        _rmq_chan = std::shared_ptr<AMQP::Channel>(new AMQP::Channel(con));
 
         _rmq_chan->declareExchange("BigStore", AMQP::topic).onError([](const char *message) {
             FATAL("{}\n", message);
@@ -171,22 +130,16 @@ namespace Rabbit
         _rmq_chan->declareQueue("Item", AMQP::durable)
             .onSuccess([]() { fmt::print("Declared queue: Item\n"); })
             .onError([](const char *message) { fmt::print("Error declaring queue Item: {}\n", message); });
+
+        _tcp_lsn->listen();
     }
 
-    void ConnectionHandler::onError(AMQP::Connection *con, const char *message)
-    {
-        (void) con;
-        FATAL("Rabbit error: {}\n", message);
-    }
+    void ConnectionHandler::onError(AMQP::Connection *, const char *message) { FATAL("Rabbit error: {}\n", message); }
 
-    void ConnectionHandler::onData(AMQP::Connection *con, const char *buffer, size_t size)
+    void ConnectionHandler::onData(AMQP::Connection *, const char *buffer, size_t size)
     {
         fmt::print("Data?\n");
-
         _tcp_con->write(const_cast<char *>(buffer), size);
-        (void) con;
-        (void) buffer;
-        (void) size;
     }
 }
 
@@ -198,5 +151,6 @@ int main(int argc, char *argv[])
     Rabbit::ConnectionHandler rabbit;
 
     fmt::print("If you keep trying hard enough, at one point you might get lucky.\n");
+    fmt::print("Alive ? {}\n", Rabbit::loop->alive());
     return rabbit.run();
 }
